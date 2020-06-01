76e93f37b733acb8805418f94e47aa784348127b
euqueme <euqueme@gmail.com>
2020-06-01 16:02:19 -0600

    Add test cases for the Exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,8 @@
 class Exercise < ApplicationRecord
+  # model association
   belongs_to :user
+  has_many :measurements, dependent: :destroy
+
+  # validations
+  validates_presence_of :name
 end
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+
+# Unit test for the Exercise model
 RSpec.describe Exercise, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure Exercise model has a 1:m relationship with the Measurement model
+  it { should have_many(:measurements).dependent(:destroy) }
+  # Validation tests
+  # ensure that the name column is present before saving
+  it { should validate_presence_of(:name) }
 end
