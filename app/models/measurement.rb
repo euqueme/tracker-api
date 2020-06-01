9616a6cb3ad079b72b5518b5e53a01ba99df96f4
euqueme <euqueme@gmail.com>
2020-06-01 16:12:58 -0600

    Add test cases for the Measurement model

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,4 +1,11 @@
 class Measurement < ApplicationRecord
+
+  # model association
   belongs_to :user
   belongs_to :exercise
+
+  # validation
+  validates_presence_of :description
+  validates_presence_of :amount
+
 end
--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+# Test suite for the Measurement model
 RSpec.describe Measurement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure a Measurement record belongs to a single Excercise record and a single User
+  it { should belong_to(:exercise) }
+  it { should belong_to(:user) }
+  # Validation test
+  # ensure description and amount columns are present before saving
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:amount) }
 end
