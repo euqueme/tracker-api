b0daa099b09505c1130bf8be5d5718f29bb02489
euqueme <euqueme@gmail.com>
2020-06-01 14:34:35 -0600

    Add test cases for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    # model association
+    has_many :exercises
+
+    # validations
+    validates_presence_of :name, :email, :password_digest, :remember_digest
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: VALID_EMAIL_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
+# Unit Test for the User model
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure User model has a 1:m relationship with the Exercise model
+  it { should have_many(:exercises) }
+  # Validation tests
+  # ensure columns name, email, password_digest and remember_digest are present before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:remember_digest) }
+
+  # ensure email has a valid format
+  describe "email" do
+    
+    addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+    addresses.each do |valid_address|
+      it { should allow_value(valid_address).for(:email) }
+    end
+
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+    addresses.each do |invalid_address|
+      it { should_not allow_value(invalid_address).for(:email) }
+    end
+  end
+  
 end
