7fda9df9c0faf9b25016a9c93f12d82789731ee8
euqueme <euqueme@gmail.com>
2020-06-02 15:22:29 -0600

    Authorize API request and add test cases for it

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,6 +1,11 @@
 module ExceptionHandler
     # provides the more graceful `included` method
     extend ActiveSupport::Concern
+
+    # Define custom error subclasses - rescue catches `StandardErrors`
+    class AuthenticationError < StandardError; end
+    class MissingToken < StandardError; end
+    class InvalidToken < StandardError; end
   
     included do
       rescue_from ActiveRecord::RecordNotFound do |e|
@@ -10,14 +15,6 @@ module ExceptionHandler
       rescue_from ActiveRecord::RecordInvalid do |e|
         json_response({ message: e.message }, :unprocessable_entity)
       end
-    end
-
-    # Define custom error subclasses - rescue catches `StandardErrors`
-    class AuthenticationError < StandardError; end
-    class MissingToken < StandardError; end
-    class InvalidToken < StandardError; end
-  
-    included do
       # Define custom handlers
       rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
       rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
