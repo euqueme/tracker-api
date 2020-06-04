2ec03f55c7a699e8500521ea0ad54614cf8ef21f
euqueme <euqueme@gmail.com>
2020-06-03 18:34:00 -0600

    Change API name to 'Exercise tracker API'

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -4,8 +4,8 @@ require 'rspec_api_documentation/dsl'
 
 RspecApiDocumentation.configure do |config|
   config.format = :json
-  config.curl_host = 'http://tracker-api' # Will be used in curl request
-  config.api_name = "Tracker API" # Your API name
+  config.curl_host = 'http://exercise-tracker-api' # Will be used in curl request
+  config.api_name = "Exercise tracker API" # Your API name
   config.request_headers_to_include = ["Host", "Content-Type"]
   config.response_headers_to_include = ["Host", "Content-Type"]
   #config.curl_headers_to_filter = ["Authorization"] # Remove this if you want to show Auth headers in request
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe Exercise, type: :model do
   # Association test
   # ensure Exercise model has a 1:m relationship with the Measurement model
-  it { should have_many(:measurements).dependent(:destroy) }
+  it { should have_many(:measurements)}
   # Validation tests
   # ensure that the name column is present before saving
   it { should validate_presence_of(:name) }
