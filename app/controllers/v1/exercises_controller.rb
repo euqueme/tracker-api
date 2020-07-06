4dbe39ebaea540921d9ec0937e4c9a326e6cf332
euqueme <euqueme@gmail.com>
2020-07-06 16:25:45 -0600

    Update images with API Version 1

---
+++ app/controllers/v1/exercises_controller.rb
@@ -0,0 +1,49 @@
+class V1::ExercisesController < ApplicationController
+  before_action :set_exercise, only: %i[show update destroy]
+  before_action :check_admin, only: %i[create update destroy]
+  skip_before_action :authorize_request, only: %i[index show]
+
+  # GET /exercises
+  def index
+    @exercises = Exercise.all
+    json_response(@exercises)
+  end
+
+  # POST /exercises
+  def create
+    @exercise = current_user.exercises.create!(exercise_params)
+    json_response(@exercise, :created)
+  end
+
+  # GET /exercises/:id
+  def show
+    json_response(@exercise)
+  end
+
+  # PUT /exercises/:id
+  def update
+    @exercise.update(exercise_params)
+    head :no_content
+  end
+
+  # DELETE /exercises/:id
+  def destroy
+    @exercise.destroy
+    head :no_content
+  end
+
+  private
+
+  def exercise_params
+    # whitelist params
+    params.permit(:name)
+  end
+
+  def set_exercise
+    @exercise = Exercise.find(params[:id])
+  end
+
+  def check_admin
+    raise(ExceptionHandler::AuthenticationError, Message.notallowed) unless current_user.admin
+  end
+end
